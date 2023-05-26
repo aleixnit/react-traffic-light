5e79a820afd0519340265a62196b784e192b288b
Aleix Aguilar <122154370+aleixnit@users.noreply.github.com>
2023-05-26 11:51:44 +0000

    Ejercicio semaforo realizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import '../../styles/index.css'
+import React, { useState } from 'react';
 
 //create your first component
 const Home = () => {
+
+	const[colorOn, setColorOn] = useState('red');
+	
+	function handelClick(e){
+		setColorOn(e.target.id)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="fondo">
+			<div className="trafficlight ">
+				<div className="protector"></div>
+				<div className="protector"></div>
+				<div className="protector"></div>
+				<div id='red'
+				style={{boxShadow: colorOn ==='red' ? '0 0 50px red' :  'none'}} 
+					onClick={handelClick} className="red"></div>
+				<div id='yellow' style={{boxShadow: colorOn ==='yellow' ? '0 0 50px yellow' : 'none'}} onClick={handelClick} className="yellow"></div>
+				<div id='green' style={{boxShadow: colorOn ==='green' ? '0 0 50px lime' : 'none'}} onClick={handelClick} className="green"></div>
+			</div>
 		</div>
 	);
 };
